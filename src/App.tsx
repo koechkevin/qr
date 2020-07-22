ca4b1c6e251acd59290735c274a20bfa1b61cbd9
Kevin Koech <koechkevin92@gmail.com>
2020-07-22 17:31:33 +0300

    Fix cors issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import QRCode from 'qrcode';
 
 function App() {
+  const [img, setImg] = useState();
+  const [value, setValue] = useState('');
+  useEffect(() => {
+    if (value) {
+      QRCode.toDataURL(value,  (err, image) => {
+        if (err) throw err;
+        setImg(image)
+      })
+    }
+  }, [value]);
+  const onChange = (e: any) => {
+    setValue(e.target.value)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ alignItems: 'center', justifyContent: 'center', display: 'flex', height: '100vh'}}>
+      <div style={{ width: 400}}>
+        <input placeholder="Enter text to generate QR code" style={{ width: '100%', outline: 0, padding: 8, borderRadius: 8, border: '1px solid lightgrey' }} onChange={onChange} value={value}/>
+      <img alt="" src={img}/>
+      </div>
     </div>
   );
 }
